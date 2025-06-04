68c2e46a52ac4c38cdf6ccdb51a8ff697187ea60
Genorius <qwerty7488@mail.ru>
2025-06-05 01:08:12 +0300

    Add files via upload

--- auth_db.py
+++ auth_db.py
@@ -1,2 +1,8 @@
-def save_user_auth(user_id, order_id): pass
-def get_order_id_by_user_id(user_id): return None
+# Заглушка для хранения авторизации
+user_auth = {}
+
+def save_user_auth(user_id, order_id):
+    user_auth[user_id] = order_id
+
+def get_order_id_by_user_id(user_id):
+    return user_auth.get(user_id)
